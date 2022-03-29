bdde86bc292a32c03383de747be4501a3969a120
yubow0 <stevewang0901@outlook.com>
2022-03-28 22:14:07 -0500

    comments of init borrower class updated

--- src/borrower/Borrower.java
+++ src/borrower/Borrower.java
@@ -1,14 +1,28 @@
 package borrower;
 
 public class Borrower {
-	int borrowerId;
-	String name;
+	int borrowerId; // a unique borrower id
+	String name;    // name of a borrower
 	
+	/*
+	 * Constructor:
+	 * parameter:
+	 * 	id: a unique borrower id
+	 * 	name: name of the borrower
+	 * return:
+	 * 	None
+	 */
 	public Borrower(int id, String name) {
 		this.borrowerId = id;
 		this.name = name;
 	}
 	
+	/*
+	 * setName: reset name for the borrower
+	 * 	newName: new name for the borrower
+	 * return:
+	 * 	None
+	 */
 	public void setName(String newName) {
 		this.name = newName;
 	}
