e44487596d3c6f8efa3b8783259339905658d169
zq0618 <71154579+zq0618@users.noreply.github.com>
2022-04-25 16:27:01 -0500

    sherry 1

--- src/borrower/BorrowerClassTest.java
+++ src/borrower/BorrowerClassTest.java
@@ -14,5 +14,16 @@ class BorrowerClassTest {
 		assertEquals(id, person1.getId());
 		assertEquals(name, person1.getName());
 	}
+	
+	@Test
+	void borrowedRecordsTest() {
+		String name = "A";
+		int id = 0;
+		Borrower person1 = new Borrower(id, name);
+		int bookId = 0;
+		person1.borrowBook(bookId);
+		assertTrue(person1.getBookBorrowed().contains(bookId));
+		assertEquals(person1.getBookBorrowed().size(), 1);
+	}
 
 }
