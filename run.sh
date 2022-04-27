ea3ac995ecffa63cddae8e3ee91e01bf3369ef29
yubow0 <stevewang0901@outlook.com>
2022-04-26 20:21:55 -0500

    sh update

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
-javac src/Books/Books.java src/borrower/Borrower.java src/library/Library.java src/source.java
+javac src/Books/Books.java src/borrower/Borrower.java src/library/Library.java src/ui/UI.java src/source.java
 cd src
 java source
